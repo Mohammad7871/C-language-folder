c74c084615f09d948afc8fd9f3f9052b59855016
Mohammad7871 <alex667871>
2025-03-10 17:35:01 +0500

    more about c

---
+++ C-notes/basic1.C
@@ -0,0 +1,19 @@
+// here we learn about all commint 
+// <<-- it is use for singlr line comment 
+//    /**/  <<--- is is used for multipel line comment
+ 
+/*
+basicly all comments are use to show detail in the code but it is not the parts of code,
+
+*/
+#include <stdio.h>
+int main (){
+    printf("hi \ni am  raza \nfrom peshawar.");
+    int  x ,y ,z;
+    x =2;
+    y = 4;
+    z = x + y;
+    printf("%d", z);
+
+    return 0;
+}
